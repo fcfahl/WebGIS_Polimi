95936401637947df5a9d85973b0c1006f8a39fa1
fcfahl <fcfahl@gmx.com>
2016-02-24 17:59:15 +0100

    jquery tabs

--- custom/jquery.js
+++ custom/jquery.js
@@ -4,50 +4,54 @@ $( document ).ready(function() {
     // Uncheck the checkboxes (problem in Firefox)
     $('input:checkbox').removeAttr('checked');
 
-    // Accordeon - expand layer panels
-    $(function() {
-
-        $( "#accordion" )
-        .accordion({
-          header: "> div > h3",
-          heightStyle: "content",
-          collapsible: false,
-          active: false,
-          icons: { "header": "ui-icon-plus", "activeHeader": "ui-icon-minus" }
-
-        })
-        .sortable({
-          axis: "y",
-          handle: "h3",
-          stop: function( event, ui ) {
-            // IE doesn't register the blur when sorting
-            // so trigger focusout handlers to remove .ui-state-focus
-            ui.item.children( "h3" ).triggerHandler( "focusout" );
-
-            // Refresh accordion to handle new order
-            $( this ).accordion( "refresh" );
-          }
-        });
-
-
-        $( "#expand" ).on( "click", function() {
-          $('.ui-widget-content').show();
-
-
-        });
+    // JQuery ui tabs
+    $( "#tabs" ).tabs({
+      heightStyle: "fill"
     });
 
+    // Accordeon - expand layer panels
+    $( "#accordion" )
+    .accordion({
+      header: "> div > h3",
+      heightStyle: "content",
+      collapsible: true,
+      active: false,
+      icons: { "header": "ui-icon-plus", "activeHeader": "ui-icon-minus" },
+
+    })
+    .sortable({
+      axis: "y",
+      handle: "h3",
+      stop: function( event, ui ) {
+        // IE doesn't register the blur when sorting
+        // so trigger focusout handlers to remove .ui-state-focus
+        ui.item.children( "h3" ).triggerHandler( "focusout" );
+
+        // Refresh accordion to handle new order
+        $( this ).accordion( "refresh" );
+      }
+    });
+
+    // Expand All button
+    $( "#expand" ).button().on( "click", function() {
+        var label = $(this).button( "option", "label" );
+        if(label === "Expand All") {
+        	$(this).button( "option", "label", "Collapse All" );
+            $('.ui-widget-content').show();
+        }
+        else {
+        	$(this).button( "option", "label", "Expand All" );
+            $('.ui-widget-content').hide();
+        }
+     });
+
+     // Setting button
+     $( "#setting" ).button().on( "click", function() {
+         var label = $(this).button( "option", "label" );
+      });
 
 
 
 
-
-    //
-    // $("#expand").button().toggle(function() {
-    //    $(this).button('option', 'label', 'Stop');
-    // }, function() {
-    //    $(this).button('option', 'label', 'Start');
-    // });
-    //
 
 });
